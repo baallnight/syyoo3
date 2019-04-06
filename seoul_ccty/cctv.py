141f42fb075469a13cf50568696d349864c7f2ee
gkdlwhdkfk <gkdldkfk@naver.com>
2019-04-06 18:25:00 +0900

    first commit

---
+++ seoul_ccty/cctv.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+
+ctx = '../data/'
+filename = 'CCTV_in_Seoul.csv'
+seoul_cctv = pd.read_csv(ctx+filename, encoding='UTF-8')
+
+#print(seoul_cctv)
+seoul_cctv_idx = seoul_cctv.columns
+#print(seoul_cctv_idx)
+
+"""
+Index(['기관명', '소계', '2013년도 이전', '2014년', '2015년', '2016년'], dtype='object')
+"""
+
+seoul_cctv.rename(columns={seoul_cctv.columns[0]: '구별'}, inplace=True)
+seoul_pop = pd.read_excel(ctx+'population_in_Seoul.xls', encoding='UTF-8',
+                          header=2, usecols='B, D, G, J, N')
+
+seoul_pop.rename(columns={seoul_pop.columns[0]: '구별'
+    ,seoul_pop.columns[1]: '인구수'
+    ,seoul_pop.columns[2]: '한국인'
+    ,seoul_pop.columns[3]: '외국인'
+    ,seoul_pop.columns[4]: '고령자'}, inplace=True)
+
+seoul_cctv.sort_values(by='소계', ascending=True).head(5)
+seoul_pop.drop([0], inplace=True)
+seoul_pop['구별'].unique()
+seoul_pop['구별'].isnull()
+
+seoul_pop.drop([26], inplace=True)
+print(seoul_pop)
